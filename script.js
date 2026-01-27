db0c27dae4366a36a9664942306b2db208143a68
Kanhaiya Roy <kanhaiyaroy1103@gmail.com>
2026-01-27 14:45:09 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("keypress",function(){
+    console.log("Game Started");
+});
+
+let button = document.querySelectorAll(".button");
+for(but of button){
+    but.addEventListener("click",function(){
+    console.log("Button was clicked");
+});
+}
