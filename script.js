45c9a40414d313ce229c1e6fc7066d7371e46fbd
Kanhaiya Roy <kanhaiyaroy1103@gmail.com>
2026-01-27 21:48:25 +0530

    updated UI

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 let gameSeq = [];
 let userSeq = [];
 
-let start = false;
+let started = false;
 let level = 0;
 
+let h3 = document.querySelector("h3");
+
 document.addEventListener("keypress",function(){
-    console.log("Game started");
+    if(started == false){
+        console.log("Game started");
+        started = true;
+
+        levelUp();
+    }
+    
 });
+
+function levelUp(){
+    level++;
+    h3.innerText = `Level ${level}`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 let button = document.querySelectorAll(".button");
 for(but of button){
